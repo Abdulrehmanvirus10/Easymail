0e5021448cc434fc9112df5d7c753dcabc8e898a
Abdul Rehman Kalsekar <60281041+Abdulrehmanvirus10@users.noreply.github.com>
2020-10-01 00:38:26 +0530

    Update secrets.py

--- main/secrets.py
+++ main/secrets.py
@@ -1,5 +1,7 @@
 # make sure the 'allow less secure apps' is turned on for your gmail account
+# It Now Takes an Input From the User Then and There itself
+import getpass
 
-MY_ADDRESS = 'your email adress here'
-MY_NAME='name that should appear in the email'
-PASSWORD = 'your email password'
+MY_ADDRESS = input(str("Please Enter Your Email Address - "))
+MY_NAME= input(str("Enter the Name You Want to See at Receivers Mailbox - "))
+PASSWORD = getpass("Please Enter Your Email Password - ")
